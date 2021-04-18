83c7a8171c36d0e07cba077c4ffdbf7b9d9002c4
kamioka24 <keee2404@gmail.com>
2021-04-18 10:11:35 +0900

    [Fix]

---
+++ app/src/main/java/com/example/basematch_kt/Player/PlayerTeamIndexActivity.kt
@@ -0,0 +1,15 @@
+package com.example.basematch_kt.Player
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.example.basematch_kt.R
+
+class PlayerTeamIndexActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player_team_index)
+    }
+}
---
+++ app/src/main/java/com/example/basematch_kt/Player/PlayerTeamShowActivity.kt
@@ -0,0 +1,12 @@
+package com.example.basematch_kt.Player
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.basematch_kt.R
+
+class PlayerTeamShowActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player_team_show)
+    }
+}
